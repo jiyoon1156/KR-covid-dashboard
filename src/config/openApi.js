f8fcfabe1379ed703995e6a57799c468bdc53713
jiyoonhur <jiyoon1156@gmail.com>
2021-06-28 16:45:51 +0900

    [FEAT] Crawler #3
    - Set config for open API
    - Set job scheduler(ing)
    - Set data for initial DB

--- src/config/openApi.js
+++ src/config/openApi.js
@@ -8,8 +8,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const today = () => {
+const yesterday = () => {
   const date = new Date();
+  const yesterdate = date.getTime() - (1*24*60*60*1000);
+  console.log(yesterdate)
+  date.setTime(yesterdate);
+
   const year = date.getFullYear();
   const month = (`0${1 + date.getMonth()}`).slice(-2);
   const day = (`0${date.getDate()}`).slice(-2);
@@ -25,7 +29,7 @@ const openApi = {
   },
   vaccine_stat: {
     method: 'get',
-    url: `https://api.odcloud.kr/api/15077756/v1/vaccine-stat?cond%5BbaseDate%3A%3AEQ%5D=${today}%2000%3A00%3A00`,
+    url: `https://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=1&perPage=10&cond%5BbaseDate%3A%3AEQ%5D=${yesterday()}%2000%3A00%3A00`,
     headers: {
       Authorization: `Infuser ${process.env.AUTH_KEY}`,
     },
--- src/jobs/agenda.js
+++ src/jobs/agenda.js
@@ -8,9 +8,9 @@ import cron from 'node-cron';
 import openApi from '../config/openApi.js';
 import apiCall from '../services/apiCall.js';
 
-const task = (time, api) => cron.schedule(time, async () => {
-  const data = await apiCall(api);
-  console.log(data);
+// const task = (time, api) => cron.schedule(time, async () => {
+//   const data = await apiCall(api);
+//   console.log(data);
   // if (api === openApi.covid_stat) {
   //   // await parsing data logic
   //   console.log('covid stat to db');
@@ -18,13 +18,13 @@ const task = (time, api) => cron.schedule(time, async () => {
   //   // await parsing data logic
   //   console.log('vaccine stat to db');
   // }
-});
+// });
 
 // task('56 23 * * *', openApi.covid_stat).start();
-task('59 22 * * *', openApi.vaccine_stat).start();
+// task('59 22 * * *', openApi.vaccine_stat).start();
 
-// const main = async () => {
-//   const temp = await apiCall(openApi.vaccine_stat);
-//   console.log(temp);
-// };
-// main();
+const main = async () => {
+  const temp = await apiCall(openApi.vaccine_stat);
+  console.log(temp);
+};
+main();
