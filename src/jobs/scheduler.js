90c30f16bf6e0664aad2b8f282cf7a1448dcfe7e
jiyoonhur <jiyoon1156@gmail.com>
2021-07-02 19:35:44 +0900

    [FEAT] Avoid duplicate data update in scheduler #5
    - Add logic to avoid duplication of data in covid API

--- src/jobs/scheduler.js
+++ src/jobs/scheduler.js
@@ -7,12 +7,24 @@ import openApi from '../config/openApi.js';
 import apiCall from '../services/apiCall.js';
 import setCovidData from '../services/setCovidData.js'
 import setVaccineData from '../services/setVaccineData.js';
+import CovidDaily from '../models/CovidDaily.js';
+import getDateRange from '../services/getDateRange.js'
 
 const task = (time, api) => cron.schedule(time, async () => {
   const data = await apiCall(api);
   if (api === openApi.covid_stat) {
-    //날짜 중복 확인 로직 필요
-    await setCovidData(data, data.length - 1);
+    const covidInfo = await CovidDaily.find({}).sort('date');
+    const len = covidInfo.length;
+
+    const oneDay = 1 * 24 * 60 * 60 * 1000;
+    const lastDate = covidInfo[len - 1].date.getTime();
+    const today = new Date().getTime();
+    if (lastDate < today) {
+      const dates = getDateRange(lastDate + oneDay, today,0);
+      for (let i = 0; i < dates.length; i++) {
+        await setCovidData(data, data.length - dates.length + i);
+      }
+    };
     console.log("=============open api covid successful============")
   } else if (api === openApi.vaccine_stat) {
     //날짜 중복 확인 로직 필요
