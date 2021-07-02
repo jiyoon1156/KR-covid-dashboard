367662c2a23d9e8bd5aee05fe28d9bdd4c1d8fbf
jiyoonhur <jiyoon1156@gmail.com>
2021-07-02 19:35:07 +0900

    [REFACTOR] Separate getDateRange function  #5
    - Separate getDateRange function in order to utilize it in another logic
    - Delete unnecessary file(insertData.js)
    - Add unusual data in covid_data.json

---
+++ src/services/getDateRange.js
@@ -0,0 +1,19 @@
+const getDateRange = (startDay, endDay, subDay) => {
+	const oneDay = 1 * 24 * 60 * 60 * 1000;
+	let startDate = startDay;
+	const endDate = endDay - subDay;
+	let dateArray = new Array();
+
+	while (startDate < endDate) {
+		const date = new Date();
+		date.setTime(startDate);
+		const year = date.getFullYear();
+  	const month = (`0${1 + date.getMonth()}`).slice(-2);
+		const day = (`0${date.getDate()}`).slice(-2);
+		dateArray.push(`${year}-${month}-${day}`);
+		startDate += oneDay
+	}
+	return dateArray
+}
+
+export default getDateRange;
